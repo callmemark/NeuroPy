2469c2ac5670c133e442e31ed93aa22fc5f214f4
Mark <65215676+callmemark@users.noreply.github.com>
2023-02-25 18:36:32 +0800

    Added initial code documentation
    Added initial code documentation

--- NeuroPy.py
+++ NeuroPy.py
@@ -47,6 +47,283 @@ class Math():
 
 
 
+
+class arrayMethods():
+	def __init__(self):
+		pass
+
+
+	def matrixMultiply(self, multiplier_arr, arr_to_mult):
+		"""
+			matrix multiplt two array
+			
+			Argumemts:
+			multiplier_arr	(List / Array)	
+			arr_to_mult		(List / Array)
+
+			Return Array
+		"""
+		output_array = []
+
+		for multiplier in multiplier_arr:
+			row_arr = []
+			for value in arr_to_mult:
+				product = multiplier * value
+				row_arr.append(product)
+
+			output_array.append(row_arr)
+
+		return output_array
+
+
+
+	def matrixVectorMultiply(self, multiplicand_arr, multiplier_arr):
+		"""
+			Calculate dot product of two array
+			
+			Argumemts:
+			multiplicand_arr	(2d matrix array) 
+			multiplier_arr		(1d vector)
+
+			Return Array
+		"""
+		output_array = []
+		print(self.getShape(multiplicand_arr),  " and ", self.getShape(multiplier_arr))
+
+		for selected_row in multiplicand_arr:
+			new_row = []
+			for index in range(len(selected_row)):
+				new_row.append(selected_row[index] * multiplier_arr[index])
+			output_array.append(new_row)
+
+		return output_array
+
+
+	def getMatrixSumOfRow(self, _2d_matrix):
+		"""
+			caculate the sum of rows of a 2d matrix array
+			
+			Argumemts:
+			_2d_matrix	(2d matrix Array)
+
+			Return Matrix Array
+		"""
+		final_arr = []
+
+		for selected_row in _2d_matrix:
+			sum_of_current_iter = 0
+			for value in selected_row:
+				sum_of_current_iter += value
+
+			final_arr.append([sum_of_current_iter])
+
+		return final_arr
+
+
+
+	def vectorMultiply(self, vector_array_01, vector_array_02):
+		"""
+			Multiply two 1d vector
+			
+			Argumemts:
+			vector_array_01	(1d vector Array)
+			vector_array_02 (1d vector Array)
+
+			Return vector array
+		"""
+		array_01_lenght = len(vector_array_01)
+		array_02_lenght = len(vector_array_02)
+
+		if array_01_lenght != array_02_lenght:
+			raise Exception("Error: Array are not equal where size is ", array_01_lenght, " != ", array_02_lenght)
+
+		output_array = []
+		loop_n = int((array_01_lenght + array_02_lenght) / 2)
+
+		for index in range(loop_n):
+			output_array.append(vector_array_01[index] * vector_array_02[index])
+
+		return output_array
+
+
+
+	def matrixAddition(self, matrix_01, matrix_02):
+		"""
+			add two matrix
+			
+			Argumemts:
+			matrix_01	(2d matrix Array)
+			matrix_02	(2d matrix Array)
+
+			Return 2d Matrix Array
+		"""
+
+		if len(matrix_01) != len(matrix_02) or len(matrix_01[0]) != len(matrix_02[0]):
+		    raise ValueError("Arrays must have the same shape")
+
+		result = [[0 for _ in range(len(matrix_01[0]))] for _ in range(len(matrix_01))]
+
+		for i in range(len(matrix_01)):
+			for j in range(len(matrix_01[0])):
+				result[i][j] = matrix_01[i][j] + matrix_02[i][j]
+
+		return result
+
+
+
+	def matrixSubtract(self, matrix_minuend, matrix_subtrahend):
+		"""
+			subtract two matrix
+			
+			Argumemts:
+			matrix_minuend	(2d matrix Array)
+			matrix_subtrahend	(2d matrix Array)
+
+			Return 2d Matrix Array
+		"""
+		if len(matrix_minuend) != len(matrix_subtrahend) or len(matrix_minuend[0]) != len(matrix_subtrahend[0]):
+			raise ValueError("Arrays must have the same shape")
+
+		result = [[0 for _ in range(len(matrix_minuend[0]))] for _ in range(len(matrix_minuend))]
+
+		for i in range(len(matrix_minuend)):
+			for j in range(len(matrix_minuend[0])):
+				result[i][j] = matrix_minuend[i][j] - matrix_subtrahend[i][j]
+
+		return result
+
+
+	def vectorSubtract(self, minuend_vector_array, subtrahend_vector_array):
+		"""
+			subtract two matrix
+			
+			Argumemts:
+			minuend_vector_array	(1d vector Array)
+			subtrahend_vector_array	(1d vector Array)
+
+			Return 1d vector Array
+		"""
+		subtracted_arr = []
+		minuend_arr_size = len(minuend_vector_array)
+		subtrahend_arr_size = len(subtrahend_vector_array)
+
+		if minuend_arr_size != subtrahend_arr_size:
+			raise Exception(str("Error on function 'vectorSubtract'. Arrays are not equal lenght with sizes " + str(minuend_arr_size) + " and " + str(subtrahend_arr_size)))
+
+		index_count = int((minuend_arr_size + subtrahend_arr_size) / 2)
+		for index in range(index_count):
+			subtracted_arr.append(minuend_vector_array[index] - subtrahend_vector_array[index])
+
+		return subtracted_arr
+
+
+	def flatten(self, matrix_arr):
+		"""
+			transform a matrix array into a 1d vector array
+			
+			Argumemts:
+			matrix_arr	(nd vector Array)
+
+			Return 1d vector Array
+		"""
+		flattened = []
+
+		if len(self.getShape(matrix_arr)) == 1:
+			return matrix_arr
+
+		for element in matrix_arr:
+		    if isinstance(element, list):
+		        flattened.extend(self.flatten(element))
+		    else:
+		        flattened.append(element)
+
+		return flattened
+
+
+	def getShape(self, array_arg):
+		"""
+			get the shape of vector / matrix array
+			
+			Argumemts:
+			array_arg	(nd Array)
+
+			Return 1d vector Array
+		"""
+		shape = []
+
+		while isinstance(array_arg, list):
+		    shape.append(len(array_arg))
+		    array_arg = array_arg[0]
+
+		return shape
+
+
+	def transpose(self, arr_arg):
+		""""
+			Transpose the given 2d matrix array swapping its elemtns positions
+
+			Arguements			:	arr_arg
+			Returns(Array) 		:	matrix array
+		"""
+
+		if len(self.getShape(arr_arg)) <= 1:
+			return arr_arg
+
+		shape = self.getShape(arr_arg)
+		transposed_list = [[None]*shape[0] for _ in range(shape[1])]
+
+		for i in range(shape[0]):
+		    for j in range(shape[1]):
+		        transposed_list[j][i] = arr_arg[i][j]
+
+		return transposed_list
+
+
+	def vectorSquare(self, vector_array):
+		""""
+			Transpose the given 2d matrix array swapping its elemtns positions
+
+			Arguements			:	arr_arg
+			Returns(Array) 		:	matrix array
+		"""
+		squared_arr = []
+		arr_shape = self.getShape(vector_array)
+		if len(arr_shape) != 1:
+			raise Exception("Error in function vectorSquare, Array should be one dimesion but have " + str(arr_shape))
+
+		for value in vector_array:
+			squared_arr.append(value ** 2)
+
+		return squared_arr
+
+
+	def vectorScalarMultiply(self, vector_array, multiplier_num):
+		""""
+			apply scalar multiplication to the given array using the given multiplier
+
+			Arguements:
+			vector_array (1d vector array)		:	1d vector array
+			multiplier_num (float) 				:	float
+
+			Returns: 1d vector array
+		"""
+		resulting_arr = []
+
+		for value in vector_array:
+			resulting_arr.append(value * multiplier_num)
+
+		return resulting_arr
+
+
+
+
+
+
+
+
+
+
+
 class Array(list):
 	def __init__(self, data):
 		"""
@@ -525,199 +802,6 @@ class ForwardPropagation(ActivationFunction):
 
 
 
-
-class arrayMethods():
-	def __init__(self):
-		pass
-
-
-	def dot_prod(self, multiplier_arr, arr_to_mult):
-		output_array = []
-
-		for multiplier in multiplier_arr:
-			row_arr = []
-			for value in arr_to_mult:
-				product = multiplier * value
-				row_arr.append(product)
-
-			output_array.append(row_arr)
-
-		return output_array
-
-
-	def multidim_arr_dotprod(self, multiplicand_arr, multiplier_arr):
-		output_array = []
-
-		for selected_row in multiplicand_arr:
-			new_row = []
-			for index in range(len(selected_row)):
-				new_row.append(selected_row[index] * multiplier_arr[index])
-			output_array.append(new_row)
-
-
-		return output_array
-
-
-
-	def getSumOfRows(self, _2d_array):
-		# get and return the sum of each rows of a 2d array 
-		final_arr = []
-
-		for selected_row in _2d_array:
-			sum_of_current_iter = 0
-			for value in selected_row:
-				sum_of_current_iter += value
-
-			final_arr.append([sum_of_current_iter])
-
-		return final_arr
-
-
-
-	def subtractArray(self, arr_01, array_02):
-		## subtact values of a 1 dimensional array
-		arr_01_lenght = len(arr_01)
-		arr_02_lenght = len(arr_02)
-
-		if arr_01_lenght != arr_02_lenght:
-			raise Exception(str("Error: Array sized are not equal where " + str(arr_01_lenght) + " != " + str(arr_02_lenght)))
-
-		difference_array = []
-		loop_count = int((array_01_lenght + arr_02_lenght) / 2)
-
-		for index in range(loop_count):
-			difference_array.append(array_01[index] - arr_02[index])
-
-		return difference_array
-
-
-
-	def multiply_array(self, array_01, array_02):
-		array_01_lenght = len(array_01)
-		array_02_lenght = len(array_02)
-
-		if array_01_lenght != array_02_lenght:
-			raise Exception("Error: Array are not equal where size is ", array_01_lenght, " != ", array_02_lenght)
-
-		output_array = []
-		loop_n = int((array_01_lenght + array_02_lenght) / 2)
-
-		for index in range(loop_n):
-			output_array.append(array_01[index] * array_02[index])
-
-		return output_array
-
-
-
-	def addArray(self, arr1, arr2):
-	    if len(arr1) != len(arr2) or len(arr1[0]) != len(arr2[0]):
-	        raise ValueError("Arrays must have the same shape")
-
-	    result = [[0 for _ in range(len(arr1[0]))] for _ in range(len(arr1))]
-
-	    for i in range(len(arr1)):
-	        for j in range(len(arr1[0])):
-	            result[i][j] = arr1[i][j] + arr2[i][j]
-
-	    return result
-
-
-	def subtractArray(self, arr1, arr2):
-		if len(arr1) != len(arr2) or len(arr1[0]) != len(arr2[0]):
-			raise ValueError("Arrays must have the same shape")
-
-		result = [[0 for _ in range(len(arr1[0]))] for _ in range(len(arr1))]
-
-		for i in range(len(arr1)):
-			for j in range(len(arr1[0])):
-				result[i][j] = arr1[i][j] - arr2[i][j]
-
-		return result
-
-
-	def subtractOneDimArray(self, minuend_array, subtrahend_array):
-		# Subtract a flatten array
-		subtracted_arr = []
-		minuend_arr_size = len(minuend_array)
-		subtrahend_arr_size = len(subtrahend_array)
-
-		if minuend_arr_size != subtrahend_arr_size:
-			raise Exception(str("Error on function 'subtractOneDimArray'. Arrays are not equal lenght with sizes " + str(minuend_arr_size) + " and " + str(subtrahend_arr_size)))
-
-		index_count = int((minuend_arr_size + subtrahend_arr_size) / 2)
-		# Loop through the lists
-		for index in range(index_count):
-			subtracted_arr.append(minuend_array[index] - subtrahend_array[index])
-
-		# returns an array with the calculated difference
-		return subtracted_arr
-
-
-
-	def flatten(self, arr):
-	    flattened = []
-
-	    if len(self.getShape(arr)) == 1:
-	    	return arr
-
-	    for element in arr:
-	        if isinstance(element, list):
-	            flattened.extend(self.flatten(element))
-	        else:
-	            flattened.append(element)
-
-	    return flattened
-
-
-	def getShape(self, array_arg):
-	    shape = []
-
-	    while isinstance(array_arg, list):
-	        shape.append(len(array_arg))
-	        array_arg = array_arg[0]
-
-	    return shape
-
-
-	def transpose(self, arr):
-	    shape = self.getShape(arr)
-	    transposed_list = [[None]*shape[0] for _ in range(shape[1])]
-	    
-	    for i in range(shape[0]):
-	        for j in range(shape[1]):
-	            transposed_list[j][i] = arr[i][j]
-	    
-	    return transposed_list
-
-
-	def square(self, arr):
-		squared_arr = []
-		arr_shape = self.getShape(arr)
-		if len(arr_shape) != 1:
-			raise Exception("Error in function square, Array should be one dimesion but have " + str(arr_shape))
-
-		for value in arr:
-			squared_arr.append(value ** 2)
-
-		return squared_arr
-
-
-
-	def weightMultiplyArr(self, arr, int_multiplier):
-		resulting_arr = []
-
-		for value in arr:
-			resulting_arr.append(value * int_multiplier)
-
-		return resulting_arr
-
-
-
-
-
-
-
-
 class Backpropagation(arrayMethods, Array):
 	def __init__(self, learning_rate = -0.01):
 		"""
@@ -739,7 +823,7 @@ class Backpropagation(arrayMethods, Array):
 			Returns: Array
 
 		"""
-		returned_value = self.subtractOneDimArray(final_output, argmaxed_final_output)
+		returned_value = self.vectorSubtract(final_output, argmaxed_final_output)
 		return Array(returned_value)
 
 
@@ -753,7 +837,7 @@ class Backpropagation(arrayMethods, Array):
 
 			Returns: Array
 		"""
-		returned_value = self.addArray(initial_weight, weight_adjustment)
+		returned_value = self.matrixAddition(initial_weight, weight_adjustment)
 		return Array(returned_value)
 
 
@@ -769,11 +853,11 @@ class Backpropagation(arrayMethods, Array):
 			Returns: Array
 
 			formula:
-			weight_ajustments = -learning_rate * [dot_prod(proceding_neuron_strenght, preceding_neuron_output)]
+			weight_ajustments = -learning_rate * [matrixMultiply(proceding_neuron_strenght, preceding_neuron_output)]
 
 		"""
 
-		neighbor_neuron_dprod = self.dot_prod(proceding_neuron_strenght, preceding_neuron_output)
+		neighbor_neuron_dprod = self.matrixMultiply(proceding_neuron_strenght, preceding_neuron_output)
 
 		final_weight = []
 		for selected_row in neighbor_neuron_dprod:
@@ -811,9 +895,9 @@ class Backpropagation(arrayMethods, Array):
 		for index in range(len(preceding_neuron_output_arr)):
 			product_arr.append(preceding_neuron_output_arr[index] * subtracted_arr[index])
 
-		dot_product_arr = self.multidim_arr_dotprod(transposed_weight, proceding_neuron_output_arr)
-		sum_of_rows_arr = self.getSumOfRows(dot_product_arr)
-		neuron_strenghts = self.multiply_array(self.flatten(sum_of_rows_arr), product_arr)
+		dot_product_arr = self.matrixVectorMultiply(transposed_weight, proceding_neuron_output_arr)
+		sum_of_rows_arr = self.getMatrixSumOfRow(dot_product_arr)
+		neuron_strenghts = self.vectorMultiply(self.flatten(sum_of_rows_arr), product_arr)
 
 		return Array(neuron_strenghts)
 
@@ -829,7 +913,6 @@ class Backpropagation(arrayMethods, Array):
 			
 			Return Array
 		"""
-		#print(Array(corresponding_layer_neurons).multiply(self.learning_rate))
 		adjusted_biase = Array(corresponding_layer_neurons).multiply(self.learning_rate)
 		return Array(adjusted_biase)
 		
@@ -848,8 +931,8 @@ class Backpropagation(arrayMethods, Array):
 
 		"""
 
-		arr_difference = self.subtractOneDimArray(ouput, labeld_output)
-		squared_arr = self.square(arr_difference)
+		arr_difference = self.vectorSubtract(ouput, labeld_output)
+		squared_arr = self.vectorSquare(arr_difference)
 
 		arr_sum = Array(squared_arr).sum()
 		e = 1 / 3 * arr_sum
